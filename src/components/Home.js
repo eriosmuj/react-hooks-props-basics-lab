9a1eafdfe82395b86c91162e532aa3d6217e9f0c
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-05-18 14:13:39 -0400

    commit tree 8957044fa3ea8cbcfc37454e6de4b7c14f1dbce7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,39 @@
 import React from "react";
 
-function Home(props) {
+
+function Home({ color, name, city}) {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color: color}}>
+        {name} is a Web Developer from {city}
       </h1>
     </div>
   );
 }
 
 export default Home;
+
+
+// import React from "react";
+
+// function Home({ color, username, city }) {
+//   return (
+//     <div id="home">
+//       <h1 style={{ color: color }}>
+//         {username} is a Web Developer from {city}
+//       </h1>
+//     </div>
+//   );
+// }
+
+// function Home(props) {
+//   return (
+//     <div id="home">
+//       <h1 style={{ color: props.color}}>
+//         {props.name} is a Web Developer from { props.city}
+//       </h1>
+//     </div>
+//   );
+// }
+
+// export default Home;
